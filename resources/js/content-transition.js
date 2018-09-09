cac0c20290809b02db242e571c314c6fe3eaa9aa
Falla Coulibaly <bestcoulibaly10@hotmail.fr>
2018-09-09 03:07:00 -0400

    Initial page transition animation

--- resources/js/content-transition.js
+++ resources/js/content-transition.js
@@ -19,5 +19,47 @@ let transitionToPage = function(pageName){
 		default:
 			break;
 	}
+	
+	
+	//$(container).removeClass(c);
 	console.log('Transitioned to page ' + pageName);
+}
+
+let callfunction = function(event){
+	console.log('transition done');
+	console.log(event);
+	let loadingScreen = document.getElementById('loadingScreen');
+	
+	loadingScreen.style.transition= '';
+	loadingScreen.style.left = '-100%';
+}
+
+var a = true;
+
+let handleclick = function(){
+	let container = document.getElementById("contentContainer");
+	let loadingScreen = document.getElementById('loadingScreen');
+	
+	loadingScreen.style.transition= 'left 1.5s';
+	
+	if (a)
+	{
+		container.style.opacity = '0.4';
+		container.style.transform = 'scale(0.8)';
+		//document.body.style.overflow='hidden';
+		loadingScreen.style.left = '100%';
+	}
+	else {
+		container.style.opacity = '1';
+		container.style.transform = 'scale(1)';
+		//document.body.style.overflow='visible';
+		loadingScreen.style.left = '200%';
+	}
+	
+	a = !a;
+	
+	container.addEventListener("webkitTransitionEnd ", callfunction,false);
+	container.addEventListener("MSTransitionEnd", callfunction,false);
+	container.addEventListener("oTransitionEnd", callfunction,false);
+	container.addEventListener("transitionend", callfunction,false);
 }
